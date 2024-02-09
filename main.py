1f7d030ae55e24697daff101a26901c15381e75b
Kacper <29923478+restaurantposse@users.noreply.github.com>
2024-02-09 13:37:30 +0100

    Barcode detection

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import cv2
-qr_detector = cv2.QRCodeDetector()
+from pyzbar.pyzbar import decode
 
-try:
-    result, _, _ = qr_detector.detectAndDecode(cv2.imread('img/img.png'))
-    print(result)
-except cv2.error as err:
-    print(f"Provided file probably does not exist. {err}")
+def qr_barcode_detect(path):
+    qr_detector = cv2.QRCodeDetector()
+    img = cv2.imread(path)
+    try:
+        result, _, _ = qr_detector.detectAndDecode(img)
+        if result == '':
+            barcode_scanner = decode(img)
+            for barcode in barcode_scanner:
+                if barcode.data != '':
+                    return barcode.data
+                else:
+                    print(f"Image doesn't contain any QR code or barcode.")
+        else:
+            return result
+    except cv2.error as err:
+        print(f"Provided file probably does not exist. {err}")
+
+if __name__ == "__main__":
+    res = qr_barcode_detect('img/img_2.png')
+    print(res)
